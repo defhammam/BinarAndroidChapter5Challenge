26c21c126f07bcb894cc3a97aba1cd766ccb8293
Hammam <hammam.mahfuzh.s@mail.ugm.ac.id>
2022-09-19 10:24:44 +0700

    Added log debugging message for each button when it's clicked using viewBinding

--- app/src/main/java/com/wkwkman/binarandroidchapter4challenge/MainActivity.kt
+++ app/src/main/java/com/wkwkman/binarandroidchapter4challenge/MainActivity.kt
@@ -2,11 +2,48 @@ package com.wkwkman.binarandroidchapter4challenge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.wkwkman.binarandroidchapter4challenge.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        initButton()
+    }
+    
+    private fun initButton(): Unit {
+        binding.apply {
+            ivPlayerRock.setOnClickListener {
+                Log.d(TAG, "initButton: Player Rock was clicked")
+            }
+            ivBotRock.setOnClickListener {
+                Log.d(TAG, "initButton: Bot Rock was clicked")
+            }
+            ivPlayerPaper.setOnClickListener {
+                Log.d(TAG, "initButton: Player Paper was clicked")
+            }
+            ivBotPaper.setOnClickListener {
+                Log.d(TAG, "initButton: Bot Paper was clicked")
+            }
+            ivPlayerScissors.setOnClickListener {
+                Log.d(TAG, "initButton: Player Scissors was clicked")
+            }
+            ivBotScissors.setOnClickListener {
+                Log.d(TAG, "initButton: Bot Scissors was clicked")
+            }
+            flReplayButton.setOnClickListener {
+                Log.d(TAG, "initButton: Replay was clicked")
+            }
+        }
+    }
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
     }
 }
 
